7ca30171bad60e97d98327aa87547a736c0e25ae
LucasDos96 <dossantos.lucas12@gmail.com>
2021-01-25 16:23:50 +0100

    initialisation mCouleur init()

--- src/main/java/org/polytechtours/javaperformance/tp/paintingants/CPainting.java
+++ src/main/java/org/polytechtours/javaperformance/tp/paintingants/CPainting.java
@@ -127,11 +127,11 @@ public class CPainting extends Canvas implements MouseListener {
 
       // initialisation de la matrice des couleurs
 
-      for (i = 0; i != mDimension.width; i++) {
-        for (j = 0; j != mDimension.height; j++) {
-          mCouleurs[i][j] = new Color(mCouleurFond.getRed(), mCouleurFond.getGreen(), mCouleurFond.getBlue());
-        }
-      }
+//      for (i = 0; i != mDimension.width; i++) {
+//        for (j = 0; j != mDimension.height; j++) {
+//          mCouleurs[i][j] = new Color(mCouleurFond.getRed(), mCouleurFond.getGreen(), mCouleurFond.getBlue());
+//        }
+//      }
     }
 
     // initialisation de la matrice de convolution : lissage moyen sur 9
